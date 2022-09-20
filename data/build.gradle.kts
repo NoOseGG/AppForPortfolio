a920acc4bf5e5c3b4acc5fa1626a92e2b7cc2f21
Yury Svirydzenka <hoc751@gmail.com>
2022-09-20 17:57:57 +0300

    changed build.gradle.kts
    changed build.gradle.kts (for data module) to kotlin, also added Dependencies: Retrofit, ConverterGson, Coroutines

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.data"
-    compileSdk = 32
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 23
-        targetSdk = 32
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -29,16 +29,20 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = libs.versions.jvmTarget.get()
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
+    testImplementation(libs.test.junit)
+    androidTestImplementation(libs.test.extJunit)
+    androidTestImplementation(libs.test.expressoCore)
+
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.converterGson)
+    implementation(libs.jetbrains.coroutines)
 }
