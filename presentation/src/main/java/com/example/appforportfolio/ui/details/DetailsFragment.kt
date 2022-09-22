31bb5e19e523d32aa92cc417c9358e00efebad10
Yury Svirydzenka <hoc751@gmail.com>
2022-09-22 23:06:55 +0300

    added DetailsFragment.kt and fragment_details.xml

---
+++ presentation/src/main/java/com/example/appforportfolio/ui/details/DetailsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.appforportfolio.ui.details
+
+import android.os.Bundle
+import android.view.View
+import com.example.appforportfolio.databinding.FragmentDetailsBinding
+import com.example.appforportfolio.ui.BaseFragment
+
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(
+    FragmentDetailsBinding::inflate
+) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
